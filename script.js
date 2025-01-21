1d24e7a217659fe815e81134f6fffe82ee4e61ff
RAMKARTHICK S <123952399+karthi0046@users.noreply.github.com>
2025-01-21 05:42:38 -0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function Close(){
-   sidebar= document.getElementById("sidebar")
-   sidebar.style.display="none"
-}
-
-function  list(){
-    listbar=document.getElementById("sidebar")
-    listbar.style.display="block"
-}
